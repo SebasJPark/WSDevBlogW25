212750b6cd8a2f432681adaeb0791084d5ff6702
SebasJPark <151964601+SebasJPark@users.noreply.github.com>
2025-01-23 22:24:34 -0500

    initial commit

---
+++ scripts/page.js
@@ -0,0 +1,10 @@
+// ========================= EECS 493 Assignment 2 Starter Code =========================
+
+// Main
+$(document).ready(function () {
+  // ====== Startup ======
+  // TODO: DEFINE YOUR JQUERY SELECTORS HERE
+  
+});
+
+// TODO: ADD YOUR EVENT HANDLERS HERE
