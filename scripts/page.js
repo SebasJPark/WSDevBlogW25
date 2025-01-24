8158480dcf94336189651cb500cf7237c837c758
SebasJPark <151964601+SebasJPark@users.noreply.github.com>
2025-01-24 00:24:54 -0500

    Everything actually works maybe

--- scripts/page.js
+++ scripts/page.js
@@ -1,10 +1,12 @@
 // ========================= EECS 493 Assignment 2 Starter Code =========================
 
-// Main
 $(document).ready(function () {
-  // ====== Startup ======
-  // TODO: DEFINE YOUR JQUERY SELECTORS HERE
-  
-});
 
-// TODO: ADD YOUR EVENT HANDLERS HERE
+  $(".accordion").on("click", function () {
+    const panel = $(this).next(".panel");
+
+    panel.slideToggle();
+
+  });
+
+});
